f157c3419bb0aceb4a1ebb61ee8785028fb42440
Niklas <niklas.buk@gmail.com>
2021-11-15 18:09:56 +0700

    #

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log(process.argv)
+
+for (let i = process.argv[2]; i <= process.argv[3]; i++) {
+   let flag = 0;
+
+   for (let j = 2; j < i; j++) {
+      if (i % j == 0) {
+         flag = 1;
+         break;
+      }
+   }
+
+   if (i > 1 && flag == 0) {
+      console.log(i);
+   }
+}
