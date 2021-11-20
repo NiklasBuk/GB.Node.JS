910dcd443b659c16b59a1664791b024a95b2246f
Niklas <niklas.buk@gmail.com>
2021-11-20 18:17:07 +0700

    node added

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
-console.log(process.argv)
+const colors = require('colors')
+
+// console.log(process.argv)
+
+const primeArr = []
+
+if (isNaN(process.argv[2]) || isNaN(process.argv[3])) {
+   console.log(colors.red('Ошибка! Введено не число!'))
+} else {
+   console.log(colors.blue(`Диапазон чисел для выборки ${process.argv[2]} - ${process.argv[3]}`))
+}
 
 for (let i = process.argv[2]; i <= process.argv[3]; i++) {
-   let flag = 0;
+   let flag = 0
 
    for (let j = 2; j < i; j++) {
       if (i % j == 0) {
-         flag = 1;
-         break;
+         flag = 1
+         break
       }
    }
 
    if (i > 1 && flag == 0) {
-      console.log(i);
-   }
+      primeArr.push(i)
+   } 
+}
+
+console.log(primeArr)
+
+if (!primeArr.length) {
+   console.log(colors.red('В указанном диапазоне нет простых чисел.'))
+} else {
+   primeArr.forEach(item => {
+      switch (primeArr.indexOf(item) % 3) {
+         case 0: 
+            console.log(colors.green(primeArr[item]))
+            break
+         case 1: 
+            console.log(colors.yellow(primeArr[item]))
+            break
+         case 2: 
+            console.log(colors.red(primeArr[item]))
+            break
+      }
+   })
 }
